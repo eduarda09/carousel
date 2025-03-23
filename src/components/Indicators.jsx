0dde58013a37440e5b4899b651497eeed043927e
eduarda09 <m.eduardakl@gmail.com>
2025-03-23 20:46:31 -0300

    Criação de carroseel de imagens

---
+++ src/components/Indicators.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Indicators = ({activeIndex, length}) => {
+  return (
+    <div className="indicators">
+        {Array.from({length}, (_,index) => (
+            <div key={index} className={index === activeIndex ? "indicator active": "indicator"}></div>
+        ))}
+
+    </div>
+  )
+}
+
+export default Indicators
